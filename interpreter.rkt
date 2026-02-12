8e5bb3f989f9553e93bc47d453bda5518efab44f
ainemt06 <ainemt06@gmail.com>
2026-02-12 00:01:33 -0500

    stuff

--- interpreter.rkt
+++ interpreter.rkt
@@ -117,27 +117,34 @@
 
 (define remove-binding
     (lambda (name state)
-        (let ([binding (lookup-binding name state)])
-             (return-state (remove-item-at-pos (index-of-binding binding) (get-state-names state))
-                           (remove-item-at-pos (index-of-binding binding) (get-state-values state))))))
+        (let* ([binding (lookup-binding name state)]
+               [index (index-of-binding binding)])
+             (return-state (remove-item-at-pos index (get-state-names state))
+                           (remove-item-at-pos index (get-state-values state))))))
 
 
 
 
-
+;; what
 ; statement list 	<statementlist> ::= <statement> <statementlist> | nothing
 ; (statement1 statement2 ...)
 ; statement 	<statement> ::= <declare> | <assign> | <return> | <if> | <while>
-; variable declaration 	<declare> ::= var <name>; | var <name> = <expression>;
+
+;; nontrivial
+; variable declaration 	<declare> ::= var <name>; | var <assign>;
 ; (var variable optional-value)
 ; assignment 	<assign> ::=  <name> = <expresson>;
 ; (= variable expression)
 ; return 	<return> ::= return <expression>;
 ; (return expression)
+
+;; hard
 ; if statement 	<if> ::= if (<condition>) <statement> | if (<condition>) <statement> else <statement>
 ; (if condition then-statement optional-else-statement)
 ; while statement 	<while> ::= while (<condition>) <statement>
 ; (while condition body-statement)
+
+;; ez
 ; expression 	<expression> ::= <condition> | <int value>
 ; condition 	<condition> ::= true | false | <name> | <condition> && <condition> | <condition> || <condition> | !<condition> | <int value> < <int value> | <int value> <= <int value> | <int value> > <int value> | <int value> >= <int value> | <expression> == <expression> | <expression> != <expression> 
 ; (&& condition condition)
