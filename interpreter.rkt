445c91b5a96e65136da66c6d4e6e666d9209245e
dborhegyi <44074532+dborhegyi@users.noreply.github.com>
2026-03-22 21:26:59 -0400

    Applied Racket prefered indentation and inline comments. Newthrow and catchthrow need comment work

--- interpreter.rkt
+++ interpreter.rkt
@@ -20,6 +20,7 @@
 ;;;; CONSTANTS/ERRORS/SIMPLE ABSTRACTIONS
 ;;;; ---------------------------------------------------------
 
+; Shortcuts for managing lists
 (define first-index 0)
 (define value-of-binding car)
 (define index-of-binding cdr)
@@ -29,6 +30,7 @@
 (define operand2 caddr)
 (define operand3 cadddr)
 
+; State/stack manipulation functions
 (define initial-state (list '() '()))
 (define acc 0)
 (define get-state-names (lambda (state) (car state)))
@@ -39,6 +41,7 @@
 (define return-state (lambda (names vals) (list names vals)))
 (define return-val (lambda (v) v))
 
+; Types of errors
 (define (type-err) (error 'type "Parameter type mismatch"))
 (define (missing-err) (error 'missing "Var not found in state"))
 (define (unbound-err) (error 'unbound "List position out of bounds"))
@@ -50,72 +53,79 @@
 (define parse-bool
   (lambda (bool)
     (if bool
-      'true
-      'false)))
+        'true
+        'false)))
 
 ;;;; ---------------------------------------------------------
 ;;;; DENOTATIONAL SEMANTICS/M_STATE FUNCTIONS
 ;;;; ---------------------------------------------------------
 
-; set up return through block and try and whatever
-
+; Execute a block of code when a bracket is encountered
 (define block-of-code
   (lambda (block state next return break continue throw)
-    (statement-list block (add-state-layer state)
-      (lambda (s) (next (remove-state-layer s)))  ; strip the layer before continuing
-      return break continue throw)))
+    (statement-list block (add-state-layer state) ; add a new layer to the state for this block
+                    (lambda (s) (next (remove-state-layer s)))  ; strip the layer before continuing
+                    return break continue throw)))
 
 ; recurse through a list of statements and update the state with each one
 (define statement-list
-    (lambda (lis state next return break continue throw)
-        (if (null? lis) 
-            (next state) ; return the final state
-            (statement (operator lis) state (lambda (new-state) (statement-list (cdr lis) new-state next return break continue throw)) return break continue throw))))
+  (lambda (lis state next return break continue throw)
+    (if (null? lis) 
+        (next state) ; return the final state
+        (statement (operator lis) state 
+                   (lambda (new-state)
+                     (statement-list (cdr lis) new-state next return break continue throw)) ; continue through the statements
+                   return break continue throw))))
 
 ; parse out the type of a statement and evaluate it (basically our M_state)
 (define statement
-    (lambda (expr state next return break continue throw)
+  (lambda (expr state next return break continue throw)
     (let ([op (operator expr)])
-        (cond
-            ((eq? op 'if) (if-statement expr state next return break continue throw))
-            ((eq? op 'while) (while expr state next return break continue throw))
-            ((eq? op 'var) (declare expr state next return break continue throw))
-            ((eq? op '=) (assign expr state next return break continue throw))
-            ((eq? op 'return) (return-statement expr state next return break continue throw))
-            ((eq? op 'begin) (block-of-code (block expr) state next return break continue throw))
-            ((eq? op 'try) (try expr state next return break continue throw))
-            ((eq? op 'throw) (throw-excep expr state next return break continue throw))
-            (else type-err)))))
+      (cond
+        ((eq? op 'if) (if-statement expr state next return break continue throw))
+        ((eq? op 'while) (while expr state next return break continue throw))
+        ((eq? op 'var) (declare expr state next return break continue throw))
+        ((eq? op '=) (assign expr state next return break continue throw))
+        ((eq? op 'return) (return-statement expr state next return break continue throw))
+        ((eq? op 'begin) (block-of-code (block expr) state next return break continue throw))
+        ((eq? op 'try) (try expr state next return break continue throw))
+        ((eq? op 'throw) (throw-excep expr state next return break continue throw))
+        (else type-err)))))
 
+; try block, depends on newnext, newreturn, newbreak
 (define try
   (lambda (expr state next return break continue throw)
     (let* ([tryblock (operand1 expr)]
-          [catchblock (operand2 expr)]
-          [exceptvar (car (operand1 catchblock))]
-          [finallyblock (operand3 expr)]) 
-          ((next (block-of-code tryblock state newnext newreturn newbreak continue newthrow))))))
+           [catchblock (operand2 expr)]
+           [exceptvar (car (operand1 catchblock))]
+           [finallyblock (operand3 expr)]) 
+      ((next (block-of-code tryblock state newnext newreturn newbreak continue newthrow)))))) ; initiate specialized next, return, beak, and throw functions
 
+; Executes the next block of code with normal next, return, break, continue, throw functions
 (define newnext
   (lambda (finallyblock s next return break continue throw)
     (block-of-code finallyblock s next return break continue throw)))
 
+; Executes the next block of code and returns a value
 (define newreturn
   (lambda (finallyblock v s next return break continue throw)
     (block-of-code finallyblock s next (lambda s2 (return v s2)) break continue throw)))
 
+; Skips to the next block of code with default extra functions
 (define newbreak
   (lambda (finallyblock s next return break continue throw)
     (block-of-code finallyblock s break return break continue throw)))
 
+; Throws exception to end the program
 (define newthrow
   (lambda (catchblock exceptvar finallyblock e s next return break continue throw)
     (block-of-code catchblock 
-           (add-binding exceptvar e s)
-           (lambda (s2) (block-of-code finallyblock s2 next return break continue throw))
-           (lambda (v s2) (block-of-code finallyblock s2 next (lambda (s3) (return v s3)) break continue throw))
-           (lambda (s2) (block-of-code finallyblock s2 break return break continue throw))
-           continue 
-           (lambda (e2 s2) (block-of-code finallyblock s2 next (lambda (s3) (throw e2 s3)) break continue throw)))))
+                   (add-binding exceptvar e s) ;update to error state
+                   (lambda (s2) (block-of-code finallyblock s2 next return break continue throw)) ; move to next
+                   (lambda (v s2) (block-of-code finallyblock s2 next (lambda (s3) (return v s3)) break continue throw)) ;return
+                   (lambda (s2) (block-of-code finallyblock s2 break return break continue throw)) ;break
+                   continue ;default continue
+                   (lambda (e2 s2) (block-of-code finallyblock s2 next (lambda (s3) (throw e2 s3)) break continue throw))))) ;throw
 
 (define catchthrow
   (lambda (finallyblock v s next return break continue throw)
@@ -128,12 +138,12 @@
 
 ; declare and optionally initialize a variable
 (define declare
-    (lambda (expr state next return break continue throw) 
-        (if (not (eq? (lookup-binding (operand1 expr) state) missing-err)) ;prevent variables from being redeclared
-            redefine-err
-            (if (= (length expr) 2)
-              (next (add-binding (operand1 expr) '() state)) ; unassigned variables default to the empty list
-              (next (add-binding (operand1 expr) (expression (operand2 expr) state) state))))))
+  (lambda (expr state next return break continue throw) 
+    (if (not (eq? (lookup-binding (operand1 expr) state) missing-err)) ;prevent variables from being redeclared
+        redefine-err
+        (if (= (length expr) 2)
+            (next (add-binding (operand1 expr) '() state)) ; unassigned variables default to the empty list
+            (next (add-binding (operand1 expr) (expression (operand2 expr) state) state))))))
 
 ;; set a variable to a value
 (define assign
@@ -142,11 +152,11 @@
 
 ; return/print the value of this statement
 (define return-statement
-    (lambda (expr state next return break continue throw)
-       (let ([val (expression (operand1 expr) state)])
-         (if (boolean? val) ; if the value is a boolean, prettify it with parse-bool
-             (return (parse-bool val))
-             (return val)))))
+  (lambda (expr state next return break continue throw)
+    (let ([val (expression (operand1 expr) state)])
+      (if (boolean? val) ; if the value is a boolean, prettify it with parse-bool
+          (return (parse-bool val))
+          (return val)))))
 
 ; evaluate one of two statements based on a condition
 (define if-statement
@@ -163,20 +173,20 @@
   (lambda (expr state next return break continue throw)
     (if (condition (operand1 expr) state) ; check the condition
         (let ([new-state (statement-list (list (operand2 expr)) state)]) ; run body of the statement
-         (while expr new-state)) ; recursively iterate through the body again
+          (while expr new-state)) ; recursively iterate through the body again
         (next state)))) ; otherwise return the state
 
 
 ; evaluate a statement
 (define expression
-    (lambda (expr state) ; evaluate the expression as a condition and an int value
-        (let ([int-binding (int-value expr state)]
-            [bool-binding (condition expr state)])
-            (if (eq? int-binding type-err) ; return the binding that is valid
-                (if (eq? bool-binding type-err)
-                    parse-err
-                    (return-val bool-binding))
-                (return-val int-binding)))))
+  (lambda (expr state) ; evaluate the expression as a condition and an int value
+    (let ([int-binding (int-value expr state)]
+          [bool-binding (condition expr state)])
+      (if (eq? int-binding type-err) ; return the binding that is valid
+          (if (eq? bool-binding type-err)
+              parse-err
+              (return-val bool-binding))
+          (return-val int-binding)))))
 
 ; evaluate an arithmetic expression
 (define int-value
@@ -215,17 +225,17 @@
       ((symbol? expr) (m-bool expr state)) ; return a variable representing a boolean
       ((list? expr)
        (let ([op (operator expr)]) ; evaluate a condition
-             (cond
-               ((eq? op '!) (not (condition (operand1 expr) state)))
-               ((eq? op '&&)  (and (condition (operand1 expr) state) (condition (operand2 expr) state)))
-               ((eq? op '||)  (or (condition (operand1 expr) state) (condition (operand2 expr) state)))
-               ((eq? op '==)  (eq? (expression (operand1 expr) state) (expression (operand2 expr) state)))
-               ((eq? op '!=)  (not (eq? (expression (operand1 expr) state) (expression (operand2 expr) state))))
-               ((eq? op '<) (< (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
-               ((eq? op '>) (> (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
-               ((eq? op '>=) (>= (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
-               ((eq? op '<=) (<= (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
-               (else type-err))))
+         (cond
+           ((eq? op '!) (not (condition (operand1 expr) state)))
+           ((eq? op '&&)  (and (condition (operand1 expr) state) (condition (operand2 expr) state)))
+           ((eq? op '||)  (or (condition (operand1 expr) state) (condition (operand2 expr) state)))
+           ((eq? op '==)  (eq? (expression (operand1 expr) state) (expression (operand2 expr) state)))
+           ((eq? op '!=)  (not (eq? (expression (operand1 expr) state) (expression (operand2 expr) state))))
+           ((eq? op '<) (< (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
+           ((eq? op '>) (> (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
+           ((eq? op '>=) (>= (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
+           ((eq? op '<=) (<= (int-value (operand1 expr) state) (int-value (operand2 expr) state)))
+           (else type-err))))
       (else type-err))))
 
 ;;;; ---------------------------------------------------------
@@ -234,33 +244,33 @@
 
 ; evaluates the integer value of a mapping
 (define m-int
-    (lambda (atom state)
-        (if (number? atom) 
-            atom ; return a literal number
-            (let ([val (value-of-binding (lookup-binding atom state))]) 
-                  (if (number? val) ; check if this var is mapped to an int
-                      val 
-                      type-err)))))   
+  (lambda (atom state)
+    (if (number? atom) 
+        atom ; return a literal number
+        (let ([val (value-of-binding (lookup-binding atom state))]) 
+          (if (number? val) ; check if this var is mapped to an int
+              val 
+              type-err)))))   
 
 ; evaluates the boolean value of a mapping
 (define m-bool
-    (lambda (atom state)
-        (cond
-          ((boolean? atom) atom)
-          ((eq? 'false atom) #f)
-          ((eq? 'true atom) #t) ; return a literal boolean
-          (else (let ([val (value-of-binding (lookup-binding atom state))])
-                 (if (boolean? val) ; check if this var is mapped to a boolean
-                     val
-                     type-err))))))   
+  (lambda (atom state)
+    (cond
+      ((boolean? atom) atom)
+      ((eq? 'false atom) #f)
+      ((eq? 'true atom) #t) ; return a literal boolean
+      (else (let ([val (value-of-binding (lookup-binding atom state))])
+              (if (boolean? val) ; check if this var is mapped to a boolean
+                  val
+                  type-err))))))   
 
 ; check if this symbol is mapped to a value
 (define m-name
-    (lambda (atom state)
-        (let ([n (lookup-binding atom state)])
-              (if (or (eq? n type-err) (eq? n missing-err) (eq? n unbound-err)) ; check if it returns an error
-                  n
-                  atom))))
+  (lambda (atom state)
+    (let ([n (lookup-binding atom state)])
+      (if (or (eq? n type-err) (eq? n missing-err) (eq? n unbound-err)) ; check if it returns an error
+          n
+          atom))))
 
 ;;;; ---------------------------------------------------------
 ;;;; BINDING OPERATIONS
@@ -281,12 +291,12 @@
           (return-state names vals)))))
 ; find the value of a binding by name
 (define lookup-binding
-    (lambda (name state)
-        (let* ([index (return-pos-of-item name (get-state-names state))]
-               [value (return-item-at-pos index (get-state-values state))])
-               (if (eq? missing-err index)
-                 missing-err
-                (cons value index)))))
+  (lambda (name state)
+    (let* ([index (return-pos-of-item name (get-state-names state))]
+           [value (return-item-at-pos index (get-state-values state))])
+      (if (eq? missing-err index)
+          missing-err
+          (cons value index)))))
 
 (define update-binding
   (lambda (name newval state)
@@ -296,11 +306,11 @@
 
 ; delete a binding
 (define remove-binding
-    (lambda (name state)
-        (let* ([binding (lookup-binding name state)]
-               [index (index-of-binding binding)])
-              (return-state (remove-item-at-pos index (get-state-names state))
-                           (remove-item-at-pos index (get-state-values state))))))
+  (lambda (name state)
+    (let* ([binding (lookup-binding name state)]
+           [index (index-of-binding binding)])
+      (return-state (remove-item-at-pos index (get-state-names state))
+                    (remove-item-at-pos index (get-state-values state))))))
 
 ;;;; ---------------------------------------------------------
 ;;;; LIST MANIPULATION HELPERS
@@ -323,12 +333,12 @@
        (let ([inner (return-pos-of-item-cps item (car lis) (lambda (v) v))])
          (if (eq? missing-err inner)
              (return-pos-of-item-cps item (cdr lis)
-               (lambda (v) (return (+ (flat-length (car lis)) v))))
+                                     (lambda (v) (return (+ (flat-length (car lis)) v))))
              (return inner))))
       ((eq? item (car lis)) (return 0))
       (else
        (return-pos-of-item-cps item (cdr lis)
-         (lambda (v) (return (+ 1 v))))))))
+                               (lambda (v) (return (+ 1 v))))))))
 
 (define return-pos-of-item
   (lambda (item lis)
@@ -364,17 +374,17 @@
          (if (< pos sublen)
              ;; Target is inside this sublist — recurse in and reconstruct
              (remove-item-at-pos-cps pos (car lis)
-               (lambda (v1 pos1)
-                 (return (cons v1 (cdr lis)) pos1)))
+                                     (lambda (v1 pos1)
+                                       (return (cons v1 (cdr lis)) pos1)))
              ;; Target is beyond this sublist — skip and recurse on rest
              (remove-item-at-pos-cps (- pos sublen) (cdr lis)
-               (lambda (v2 pos2)
-                 (return (cons (car lis) v2) pos2))))))
+                                     (lambda (v2 pos2)
+                                       (return (cons (car lis) v2) pos2))))))
       ((zero? pos) (return (cdr lis) 0))
       (else
        (remove-item-at-pos-cps (- pos 1) (cdr lis)
-         (lambda (v pos)
-           (return (cons (car lis) v) pos)))))))
+                               (lambda (v pos)
+                                 (return (cons (car lis) v) pos)))))))
 
 (define remove-item-at-pos
   (lambda (pos lis)
@@ -389,16 +399,16 @@
        (let ([sublen (flat-length (car lis))])
          (if (< pos sublen)
              (replace-item-at-pos-cps pos item (car lis)
-               (lambda (v1 pos1)
-                 (return (cons v1 (cdr lis)) pos1)))
+                                      (lambda (v1 pos1)
+                                        (return (cons v1 (cdr lis)) pos1)))
              (replace-item-at-pos-cps (- pos sublen) item (cdr lis)
-               (lambda (v2 pos2)
-                 (return (cons (car lis) v2) pos2))))))
+                                      (lambda (v2 pos2)
+                                        (return (cons (car lis) v2) pos2))))))
       ((zero? pos) (return (cons item (cdr lis)) 0))
       (else
        (replace-item-at-pos-cps (- pos 1) item (cdr lis)
-         (lambda (v pos)
-           (return (cons (car lis) v) pos)))))))
+                                (lambda (v pos)
+                                  (return (cons (car lis) v) pos)))))))
 
 (define replace-item-at-pos
   (lambda (pos item lis)
